3bdf2a8287a749330056f50a6fa425ad2db165ee
akriti <akritikts@gmail.com>
2017-01-19 13:49:57 +0530

    Initial commit

---
+++ app/src/main/java/in/silive/selfiegeek/Activities/SplashScreen.java
@@ -0,0 +1,60 @@
+package in.silive.selfiegeek.Activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import in.silive.selfiegeek.R;
+
+public class SplashScreen extends AppCompatActivity {
+    RelativeLayout splash;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        context = getApplicationContext();
+        splash = (RelativeLayout)findViewById(R.id.splash);
+        checkConnection();
+    }
+    public void checkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+        if (info == null) {
+            //   Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show();
+            //no_net_connection.setVisibility(View.VISIBLE);
+            Snackbar snackbar = Snackbar
+                    .make(splash, "No internet connection!", Snackbar.LENGTH_INDEFINITE);
+// Changing message text color
+            snackbar.setActionTextColor(Color.RED);
+// Changing action button text color
+            View sbView = snackbar.getView();
+            TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+            textView.setTextColor(Color.YELLOW);
+            snackbar.show();
+        }
+        else{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 4000);
+        }
+
+
+
+    }
+}
