5ab10ae0f6efaca1ebf854d9adfb6a697b20c43d
akriti <akritikts@gmail.com>
2017-01-21 18:40:49 +0530

    access camera

--- app/src/main/java/in/silive/selfiegeek/Activities/MainActivity.java
+++ app/src/main/java/in/silive/selfiegeek/Activities/MainActivity.java
@@ -1,15 +1,84 @@
 package in.silive.selfiegeek.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import in.silive.selfiegeek.R;
 
 public class MainActivity extends AppCompatActivity {
+    Button cam,video;
+    private Camera camera;
+    private int cameraId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TAG","MainActivity created");
+        cam = (Button)findViewById(R.id.cam);
+        cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TAG","camera clicked");
+            }
+        });
+        video = (Button)findViewById(R.id.video);
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TAG","video clicked");
+            }
+        });
+        Log.d("TAG","layout for MainActivity initialised");
+        checkCameraHardware(getApplicationContext());
+
     }
+    private void checkCameraHardware(Context context) {
+        Log.d("TAG","checkCameraHardware called from MainActivity");
+        if (!getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            Toast.makeText(this, "No camera on this device", Toast.LENGTH_LONG)
+                    .show();
+        } else {
+            cameraId = findFrontFacingCamera();
+            if (cameraId < 0) {
+                Toast.makeText(this, "No front facing camera found.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                camera = Camera.open(cameraId);
+            }
+        }
+    }
+
+    private int findFrontFacingCamera() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                Log.d("TAG", "Camera found");
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+    @Override
+    protected void onPause() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+        super.onPause();
+    }
+
 }
--- app/src/main/java/in/silive/selfiegeek/Activities/SplashScreen.java
+++ app/src/main/java/in/silive/selfiegeek/Activities/SplashScreen.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -23,16 +24,19 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Log.d("TAG","SplashScreen created");
         context = getApplicationContext();
         splash = (RelativeLayout)findViewById(R.id.splash);
         checkConnection();
     }
     public void checkConnection() {
+        Log.d("TAG","checkConnection called from Splash");
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo info = connectivityManager.getActiveNetworkInfo();
         if (info == null) {
             //   Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show();
             //no_net_connection.setVisibility(View.VISIBLE);
+            Log.d("TAG","No net connection : inside checkConnection from SplashScreen");
             Snackbar snackbar = Snackbar
                     .make(splash, "No internet connection!", Snackbar.LENGTH_INDEFINITE);
 // Changing message text color
